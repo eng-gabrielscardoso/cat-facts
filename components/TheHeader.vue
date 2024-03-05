b25d07b6a5e5e3697cc172cceb2495d0b9ebe17b
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-03-05 11:47:03 -0300

    basic project organisation

---
+++ components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { company } = useAppConfig()
+</script>
+
+<template>
+  <header>
+    <h1>
+      The {{ company }} cat facts page
+    </h1>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, at eaque doloremque voluptas, quo dignissimos corporis id vitae exercitationem eveniet ullam odit fugit necessitatibus? Reprehenderit vel nulla quibusdam consectetur illo.
+  </div>
+</template>

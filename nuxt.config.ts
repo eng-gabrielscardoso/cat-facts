6cb04e9ef31cac76726e64723ed6872b91738d92
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-03-05 14:53:32 -0300

    add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxtjs/google-fonts',
     'nuxt-lodash',
-    'nuxt-icon'
+    'nuxt-icon',
   ],
 
   devtools: { enabled: true },
+
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Jost: true,
+      jost: true,
+    }
+  }
 })
